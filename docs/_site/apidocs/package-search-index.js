ee44283a2bef48e21878c2214d4035d0f78821f8
Bryan Stopp <bryan.stopp@gmail.com>
2021-11-23 09:49:45 -0600

    Delete generated content

--- docs/_site/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.adobe.aem.modernize"},{"l":"com.adobe.aem.modernize.component"},{"l":"com.adobe.aem.modernize.policy"},{"l":"com.adobe.aem.modernize.rule"},{"l":"com.adobe.aem.modernize.structure"}]
